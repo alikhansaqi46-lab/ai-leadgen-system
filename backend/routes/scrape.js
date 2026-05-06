365004ae677b8da66249ba14cc6962c7aee88a36
Saqib Ali <alikhansaqi46@gmail.com>
2026-05-06 20:24:16 +0800

    final fix api + puppeteer route

--- backend/routes/scrape.js
+++ backend/routes/scrape.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const puppeteer = require("puppeteer");
 
-router.get("/", async (req, res) => {
+router.get("/scrape", async (req, res) => {
   try {
     const { keyword, location } = req.query;
 
